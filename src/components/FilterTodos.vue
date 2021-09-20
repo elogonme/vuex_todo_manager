684e9da586d571d3d83d4fe140aa87a82cc39bd8
Eldar Humbatov <elogon@gmail.com>
2021-09-20 10:27:07 -0400

    Working Todo with vuex

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    Filter Todos:
+    <select v-model="limit" @change="setFilter($event)">
+      <option value="200">200</option>
+      <option value="100">100</option>
+      <option value="50">50</option>
+      <option value="20">20</option>
+      <option value="10">10</option>
+      <option value="5">5</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "FilterTodos",
+  data() {
+    return {
+      limit: 200,
+    };
+  },
+  methods: {
+    ...mapActions(["filterTodos"]),
+    setFilter(e) {
+      this.filterTodos(e);
+    },
+  },
+};
+</script>
+    
+<style scoped>
+select {
+  margin-top: 20px;
+  padding: 6px;
+  border: #41b883 1px solid;
+}
+</style>
